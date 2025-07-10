ddc7a18219644f6a65559e85b3f10ef4ab9fe754
SHASHANK PANDEY <shashankpandey201085@gmail.com>
2025-07-10 17:35:44 +0530

    Delete backend directory

--- backend/models/history.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const historySchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
-  song: { type: mongoose.Schema.Types.ObjectId, ref: "songs", required: true },
-  playedAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("history", historySchema);
