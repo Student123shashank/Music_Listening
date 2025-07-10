1d7e7d542c8d931f9d83a24292895f83d27214cf
SHASHANK PANDEY <shashankpandey201085@gmail.com>
2025-07-10 17:35:55 +0530

    Delete frontend directory

--- frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-// src/services/api.js
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:1000", // fallback
-});
-
-export default API;
